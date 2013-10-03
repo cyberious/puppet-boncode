8497406e8a5df3cbcecfc3e0d645e4a20e9a27a5
Travis Fields <tfields@commercehub.com>
2013-10-03 14:16:38 -0400

    updated for production, removed files and went all templates

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,5 @@
-# == Class: BonCode
+
+# == Class: boncode
 #
 # Full description of class BonCode here.
 #
@@ -36,52 +37,65 @@
 # Copyright 2013 Your name here, unless otherwise noted.
 #
 class boncode {
-       	$installDir = "D:/software/"
-        $sourceDir  = '\\storage1\files\shared\tfields'
-        $port = 18009
-        $host = "localhost"
-        $version = "v1014"
-	
-	file{"C:\inetpub\wwwroot\BIN\BonCodeAJP13.settings":
-                source  => 'puppet:///modules/boncode/BonCode-site.settings',
-		require => Package['BonCode AJP 1.3 Connector']
-        }
-        file{"C:\inetpub\wwwroot\web.config":
-                source  => 'puppet:///modules/boncode/web.config',
-		require => Package['BonCode AJP 1.3 Connector']
-        }
-        file{"C:\windows\BonCodeAJP13.settings":
-                source  => 'puppet:///modules/boncode/BonCodeRoot.settings',
-		require	=> Package['BonCode AJP 1.3 Connector']
-        }
+  $installDir               = hiera("boncode::installDir","D:/software/")
+  $port                     = hiera("boncode::tomcatAjpPort",18009)
+  $host                     = hiera("boncode::hostname","localhost")
+  $customErrors             = hiera("boncode::customErros","FALSE")
+  $execTimeout              = hiera("boncode::execTimeout",720)
+  $version                  = hiera("boncode::version","v1014")
+  $packetSize               = hiera("boncode::packetSize",65536)
+  $enableHTTPStatusCodes    = hiera("boncode::enableHttpStatusCodes","False")
+  $enableHeaderDataSupport  = hiera("boncode::enableHeaderDataSupport","True")
+  $enableRemoteAdmin        = hiera("boncode::enableRemoteAdmin","False")
+  $allowEmptyHeaders        = hiera("boncode::allowEmptyHeaders","True")
+  $maxConnections           = hiera("boncode::maxConnections",500)
+  $flushThreshold           = hiera("boncode::fulshThreshold",0)
+  $writeTimeOut             = hiera("boncode::writeTimeOut",30000)
+  $logDir                   = hiera("boncode::logDir",'d:\log')
+  $logLevel                 = hiera("boncode::logLevel",2)
 
-        file {"${installDir}/BonCodeAjp13_${version}.zip":
-                source  => "puppet:///modules/boncode/AJP13_${version}.zip",
-                notify  => Exec['ExpandAjpZip']
-        }
+  file{"C:\inetpub\wwwroot\BIN\BonCodeAJP13.settings":
+    content  => template('boncode/BonCodeSite.settings.erb'),
+    require => Package['BonCode AJP 1.3 Connector']
+  }
 
-        file {"BonCodeUnzip":
-                path    => "${installDir}/Unzip.ps1",
-                source  => "puppet:///modules/boncode/Unzip.ps1"
-        }
+  file{"C:\inetpub\wwwroot\web.config":
+    content  => template('boncode/web.config.erb'),
+    require => Package['BonCode AJP 1.3 Connector']
+  }
 
-        exec {'ExpandAjpZip':
-                provider=> powershell,
-                command => "${installDir}\Unzip.ps1 -InstallDir ${installDir} -Version ${version}",
-                refreshonly     => true,
-                require => File['BonCodeUnzip']
-        }
-        file {'BonCodeInstallSettings':
-                path    => "D:/software/BonCode/${version}/installer.settings",
-		ensure	=> file,
-                content => template("boncode/installer.settings.erb")
-        }
-        package {'BonCode AJP 1.3 Connector':
-                provider  =>  windows,
-                ensure    =>  installed,
-                source    =>  "D:/Software/Boncode/${version}/Connector_Setup.exe",
-                install_options => ['/VERYSILENT','/SUPPRESSMSGBOXES','/LOG','/SP-','/NOCANCEL','/NORESTART']
-        }
+  file{"C:\windows\BonCodeAJP13.settings":
+    content  => template('boncode/BonCodeRoot.settings.erb'),
+    require => Package['BonCode AJP 1.3 Connector']
+  }
 
+  file {"${installDir}/BonCodeAjp13_${version}.zip":
+    source  => "puppet:///modules/boncode/AJP13_${version}.zip",
+    notify  => Exec['ExpandAjpZip']
+  }
 
+  file {"BonCodeUnzip":
+    path    => "${installDir}/Unzip.ps1",
+    source  => "puppet:///modules/boncode/Unzip.ps1"
+  }
+
+  exec {'ExpandAjpZip':
+    provider=> powershell,
+    command => "${installDir}\Unzip.ps1 -InstallDir ${installDir} -Version ${version}",
+    refreshonly     => true,
+    require => File['BonCodeUnzip']
+  }
+
+  file {'BonCodeInstallSettings':
+    path    => "D:/software/BonCode/${version}/installer.settings",
+    ensure  => file,
+    content => template("boncode/installer.settings.erb")
+  }
+
+  package {'BonCode AJP 1.3 Connector':
+    provider  =>  windows,
+    ensure    =>  installed,
+    source    =>  "D:/Software/Boncode/${version}/Connector_Setup.exe",
+    install_options => ['/VERYSILENT','/SUPPRESSMSGBOXES','/LOG','/SP-','/NOCANCEL','/NORESTART']
+  }
 }
